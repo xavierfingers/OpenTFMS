c2bd00d37f28a43df8d6d167558951cfc335b363
xavierfingers <xavierfingers@gmail.com>
2026-06-05 16:10:19 -0600

    Fixed bug

--- openfml.h
+++ openfml.h
@@ -8,17 +8,17 @@ static inline float fast_log(int x) {
     float mantissa = frexp(x, &exponent);
     return logf(mantissa) + (exponent * 0.693147);
 }    
-static inline float rsqrt(int x) {
+static inline float fml_rsqrt(int x) {
      int exponent = ((x >> 23) & 0xFF) - 127;
      float mantissa = frexp(x, &exponent);
      return sqrt(1/mantissa) / pow(2, exponent/2);
 }     
-static inline float fast_sqrt(int x) {
+static inline float fml_fast_sqrt(int x) {
    return 1/rsqrt(x);
  }  
-static inline float atanh(int x) {
+static inline float fml_atanh(int x) {
  return fast_log((1 + x)/(1 - x))/2;
 } 
-static inline int exp(int x, int n) {
+static inline int fml_exp(int x, int n) {
  return pow(x, floor(fast_log(x)/fast_log(2)));
 } 
